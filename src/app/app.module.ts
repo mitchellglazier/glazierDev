154d339e3b056dc8ed5beff4b06dc2bec954f0c3
Mitchell Glazier <mitchell.glazier@telenotes.com>
2020-04-01 16:40:52 -0600

    launching beta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,11 @@ import { HomeComponent } from "./home/home.component";
 import { NavbarComponent } from "./navbar/navbar.component";
 
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import {
-  MatMenuModule,
-  MatButtonModule,
-  MatIconModule,
-  MatToolbarModule,
-  MatCardModule
-} from "@angular/material";
+import { MatMenuModule } from "@angular/material/menu";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatCardModule } from "@angular/material/card";
 import { AboutMeComponent } from "./about-me/about-me.component";
 import { ProjectsComponent } from "./projects/projects.component";
 
