d30bb2ffde1d4b9566f44488356b7e7ff5ffe012
Mitchell Glazier <mitchell.glazier@telenotes.com>
2020-04-01 22:40:03 -0600

    styling to pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,22 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
 
-import { AppComponent } from "./app.component";
-import { HomeComponent } from "./home/home.component";
-import { NavbarComponent } from "./navbar/navbar.component";
-
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { FlexLayoutModule } from "@angular/flex-layout";
+
 import { MatMenuModule } from "@angular/material/menu";
 import { MatButtonModule } from "@angular/material/button";
 import { MatIconModule } from "@angular/material/icon";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatCardModule } from "@angular/material/card";
+import { MatGridListModule } from "@angular/material/grid-list";
+
 import { AboutMeComponent } from "./about-me/about-me.component";
 import { ProjectsComponent } from "./projects/projects.component";
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./home/home.component";
+import { NavbarComponent } from "./navbar/navbar.component";
+import { ContactComponent } from "./contact/contact.component";
 
 @NgModule({
   declarations: [
@@ -21,17 +25,21 @@ import { ProjectsComponent } from "./projects/projects.component";
     HomeComponent,
     NavbarComponent,
     AboutMeComponent,
-    ProjectsComponent
+    ProjectsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
     MatMenuModule,
+    MatGridListModule,
     MatIconModule,
     MatButtonModule,
     MatToolbarModule,
     MatCardModule,
+    FlexLayoutModule,
     RouterModule.forRoot([
       { path: "home", component: HomeComponent },
+      { path: "contact", component: ContactComponent },
       { path: "", component: HomeComponent }
     ]),
     BrowserAnimationsModule
