18b80b6d38d688738b55013a8935e2f3ae6024bd
Mitchell Glazier <mitchell.glazier@telenotes.com>
2020-04-10 20:02:31 -0600

    added more login features

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,36 @@
 import { Component, OnInit, OnDestroy } from "@angular/core";
 import { AuthenticationService } from "./authentication.service";
+import { Observable } from "rxjs";
+import { AngularFireAuth } from "@angular/fire/auth";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   showMenu = false;
-  date = new Date();
+  today = new Date();
+  date =
+    this.today.getMonth() +
+    1 +
+    "/" +
+    this.today.getDate() +
+    "/" +
+    this.today.getFullYear();
+  user;
+  currentUser;
 
-  constructor(public authService: AuthenticationService) {}
+  constructor(
+    public authService: AuthenticationService,
+    private firebaseAuth: AngularFireAuth
+  ) {}
+
+  ngOnInit() {
+    this.firebaseAuth.authState.subscribe(user => {
+      this.user = user;
+    });
+  }
 
   toggleMenu() {
     this.showMenu = !this.showMenu;
